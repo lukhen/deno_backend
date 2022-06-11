8c114692579af833a54965aa9251ef24e3d814fb
lukhen <lukasz.hen@gmail.com>
2022-06-11 08:20:36 +0200

    getUserHandler2 can handle fetching a user as long as his name is user1.

--- app.ts
+++ app.ts
@@ -25,10 +25,12 @@ const getUser: (name: string) => TE.TaskEither<string, User> =
     name => TE.right(LUKH)
     
 
+
+type getUserDbFT = (db: Record<string, User>) => (name: string) => TE.TaskEither<string, User>
 /**
 !!!
 */
-const getUserDb: (db: Record<string, User>) => (name: string) => TE.TaskEither<string, User> =
+const getUserDb: getUserDbFT =
     db => name => {	
 	return db[name] ? TE.right(db[name]) : TE.left("no such user")
     }
@@ -91,7 +93,6 @@ const getUserHandler: (req: Request) => TE.TaskEither<string, Response> =
 	    )
 	)
     }
-
 Deno.test("request to fetch the user named lukh", async () => {
     const req = new Request("https://example.com/users/lukh", {method: "GET"})    
     const test = await pipe(
@@ -117,5 +118,44 @@ Deno.test("request to fetch the user named lukh", async () => {
     )() as () => void
     
     test()
+})
+
+const getUserHandler2: (getUser: (name: string) => TE.TaskEither<string, User>) => (req: Request) => TE.TaskEither<string, Response> =
+    getUser => request => {
+	return pipe(
+	    getUser("user1"),
+	    TE.map(
+		u => new Response(JSON.stringify(u))
+	    )
+	)
+    }
+
+Deno.test("getUserHandler2, user1 exists", async () => {
+    const req = new Request("https://example.com/users/user1", {method: "GET"})    
+    const getUser = getUserDb({user1: {name: "user1"}})
+    const test = await pipe(
+	TC.fromPairOfSums(
+	    pipe(
+		getUserHandler2(getUser)(req),
+		TE.chain(r => TE.tryCatch(
+		    () => r.json() as Promise<User>,
+		    reason => `${reason}`
+		)),
+	    ),
+	    getUser("user1")
+	),
+	TC.fold(
+	    ([e1, e2]) => T.of(() => {fail("this should not be reached 1")}),
+	    (_) => T.of(() => {fail("this should not be reached 2")}),
+	    (_) => T.of(() => {fail("this should not be reached 3")}),
+	    ([resp_user, fetched_user]) => T.of(() => {
+		assertEquals(resp_user, fetched_user)
+	    })
+	    
+	)
+    )() as () => void
+    
+    test()
+
 })
 
