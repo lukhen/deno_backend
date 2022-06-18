6ff725a1225b6b7a19aa52d81b645b28021adc92
lukhen <lukasz.hen@gmail.com>
2022-06-18 16:28:27 +0200

    Confirmed that getClientsHandler works for many clients.

--- findClientsHandler.test.ts
+++ findClientsHandler.test.ts
@@ -100,4 +100,41 @@ Deno.test("one", async () => {
     x()
 })
 
+Deno.test("many", async () => {
+    const url = "https://example.com/clients/smith"
+    const req = new Request(url, {method: "GET"})
+    const findOneClient: findClientsFT =
+	name => TE.right([
+	    {name: "John Smith", address: "Polna 1", email: "john@smith.js", phone: "112233"},
+	    {name: "Agent Smith", address: "Matrix", email: "agent@smith.js", phone: "554433"}
+	])
+
+    const x = await pipe(
+	TC4.fromPairOfSums(
+	    pipe(
+		findClientsHandler(findOneClient)(req),
+		TE.chain(r => TE.tryCatch(
+		    () => r.json() as Promise<Client[]>,
+		    reason => `${reason}`
+		))
+	    ),
+	    pipe(
+		getClientsNameFromUrl(url),
+		O.map(findOneClient),
+		O.match(
+		    () => TE.left("no client's name in url"),
+		    x => x
+		)
+	    )
+	),
+	TC4.fold(
+	    ([e1, e2]) => T.of(() => {fail(`${e1}, ${e2}`)}),
+	    ([e, clients]) => T.of(() => {fail(`${e}, ${clients}`)}),
+	    ([clients, e]) => T.of(() => {fail(`${clients}, ${e}`)}),
+	    ([clients1, clients2]) => T.of (() => {assertEquals(clients1, clients2)})
+	)
+    )() as () => void
+    x()
+})
+
 
